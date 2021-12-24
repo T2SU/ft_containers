81533c217e7c866a7c2441c55fa223990ff3c384
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-12-24 23:00:53 +0900

    1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 19:04:19 by smun              #+#    #+#             */
-/*   Updated: 2021/12/24 22:49:19 by smun             ###   ########.fr       */
+/*   Updated: 2021/12/24 23:00:31 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ static void	TestVector()
 
 	ft::UnitTest<ExpectedContTest, YourContTest> ut1;
 
+	/* Test contents are belongs to following files:
+	**   ./tests/vector/{TEST_NAME}.ipp
+	*/
+
 	BEGIN_EVAL;
 	REGISTER_EVAL(1, ut1, BasicTest1);
 	REGISTER_EVAL(2, ut1, BasicTest2);
 	REGISTER_EVAL(3, ut1, Resize);
 	END_EVAL;
-
-
 }
 
 int main(void)
