5ef81948e3d1a073de769d6bc4cdc15b927aa7c1
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-01-14 19:49:49 +0900

    doublered 17-25

--- experiments/red_black_tree_1.cpp
+++ experiments/red_black_tree_1.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   binary_search_tree.cpp                             :+:      :+:    :+:   */
+/*   red_black_tree_1.cpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 22:44:03 by smun              #+#    #+#             */
-/*   Updated: 2022/01/14 16:23:52 by smun             ###   ########.fr       */
+/*   Updated: 2022/01/14 19:45:23 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ namespace ft
 		class node
 		{
 		private:
-			Compare _comp;
+			Compare	_comp;
+			tree*	_tree;
 
 			node();
 			node(node const& o);
@@ -43,7 +44,11 @@ namespace ft
 			void	transplant(node* x)
 			{
 				if (_parent == nullptr)
+				{
+					_tree->_root = x;
+					x->_parent = nullptr;
 					return;
+				}
 				if (_parent->_left == this)
 					_parent->setLeftChild(x);
 				else if (_parent->_right == this)
@@ -58,7 +63,11 @@ namespace ft
 					std::cout << ' ';
 				if (level == 0)
 					std::cout << "-:";
-				std::cout << _value << std::endl;
+				std::cout << _value << ' ';
+				if (_black)
+					std::cout << "(B)" << std::endl;
+				else
+					std::cout << "(R)" << std::endl;
 				if (_left)
 				{
 					++level;
@@ -104,14 +113,109 @@ namespace ft
 					if (_left)
 						return (_left->insert(value));
 					else
-						return (_left = new node(value, this));
+						return (_left = new node(_tree, value, this));
 				}
 				else
 				{
 					if (_right)
 						return (_right->insert(value));
 					else
-						return (_right = new node(value, this));
+						return (_right = new node(_tree, value, this));
+				}
+			}
+
+			bool	isDoubleRed()
+			{
+				node* p = _parent;
+				if (p == nullptr)
+					return false;
+				return !isBlack() && !p->isBlack();
+			}
+
+			node*	getUncleNode()
+			{
+				node* p = _parent;
+				if (p == nullptr)
+					return nullptr;
+				node* grandp = p->_parent;
+				if (grandp == nullptr)
+					return nullptr;
+				if (grandp->_left == p)
+					return grandp->_right;
+				else
+					return grandp->_left;
+			}
+
+			void	tryRecolouring()
+			{
+				if (isRootNode())
+				{
+					if (!isBlack())
+						_black = true;
+					return;
+				}
+
+				node* p = _parent;
+				node* g = _parent->_parent;
+				node* u = getUncleNode();
+
+				if (g == nullptr || !isDoubleRed())
+					return;
+				if (u == nullptr || u->isBlack())
+					return;
+
+				u->_black = true;
+				p->_black = true;
+				g->_black = false;
+				g->tryRecolouring();
+			}
+
+			void	tryRestructuring()
+			{
+				if (isRootNode())
+					return;
+
+				node* p = _parent;
+				node* g = _parent->_parent;
+				node* u = getUncleNode();
+
+				if (g == nullptr || !isDoubleRed())
+					return;
+				if (u != nullptr && !u->isBlack())
+					return;
+
+				node* tab[] = {this, p, g};
+				for (int i = 0; i < 2; i++)
+					for (int j = 0; j < 2 - i; j++)
+						if (_comp(tab[j + 1]->_value, tab[j]->_value))
+							std::swap(tab[j], tab[j + 1]);
+
+				// LL: x, p, g
+				// LR: p, x, g
+				// RL: g, x, p
+				// RR: g, p, x
+
+				if (tab[0] == this) // LL
+				{
+					p->rightRotate();
+					std::swap(p->_black, g->_black);
+				}
+				else if (tab[1] == this && tab[0] == p) // LR
+				{
+					this->leftRotate();
+					this->rightRotate();
+					std::swap(this->_black, g->_black);
+				}
+				else if (tab[1] == this) // RL
+				{
+					this->rightRotate();
+					this->leftRotate();
+					std::swap(this->_black, g->_black);
+				}
+				else if (tab[2] == this) // RR
+				{
+					p->leftRotate();
+					std::swap(p->_black, g->_black);
 				}
 			}
 
@@ -120,6 +224,16 @@ namespace ft
 			bool	isChild(node* node)
 			{
 				return _left == node || _right == node;
+			}
+
+			bool	isBlack()
+			{
+				return _black;
+			}
+
+			bool	isRootNode()
+			{
+				return _parent == nullptr;
 			}
 
 			void	leftRotate()
@@ -143,19 +257,25 @@ namespace ft
 			}
 
 		public:
-			T _value;
-			node* _parent;
-			node* _left;
-			node* _right;
+			T		_value;
+			node*	_parent;
+			node*	_left;
+			node*	_right;
+			bool	_black;
 
 			virtual ~node() {}
-			node(T const& value, node* parent)
+			node(tree* tree, T const& value, node* parent)
 				: _value(value)
 				, _parent(parent)
 				, _left(nullptr)
 				, _right(nullptr)
+				, _black(false)
 				, _comp(Compare())
-			{}
+				, _tree(tree)
+			{
+				if (parent == nullptr)
+					_black = true;
+			}
 
 			friend class tree;
 		};
@@ -173,9 +293,15 @@ namespace ft
 		void	insert(T const& value)
 		{
 			if (_root)
-				_root->insert(value);
+			{
+				node* inserted = _root->insert(value);
+				inserted->tryRecolouring();
+				inserted->tryRestructuring();
+			}
 			else
-				_root = new node(value, nullptr);
+			{
+				_root = new node(this, value, nullptr);
+			}
 		}
 
 		void	print()
@@ -197,33 +323,26 @@ int main()
 {
 	ft::tree<int> tree;
 	std::cout << "hello tree" << std::endl;
-	tree.insert(7);
-	tree.insert(4);
-	tree.insert(3);
-	tree.insert(6);
-	tree.insert(2);
-	tree.insert(11);
-	tree.insert(9);
+
+	/*tree.insert(8);
 	tree.insert(18);
-	tree.insert(14);
-	tree.insert(12);
+	tree.insert(5);
+	tree.insert(15);
 	tree.insert(17);
-	tree.insert(19);
-	tree.insert(22);
+	tree.insert(25);
+	tree.insert(40);
+	tree.insert(80);*/
+	tree.insert(8);
+	tree.insert(18);
+	tree.insert(5);
+	tree.insert(15);
+	tree.insert(17);
+	tree.insert(25);
+	tree.insert(40);
+	tree.insert(80);
+
+
 	tree.print();
-
-	std::cout << std::endl << std::endl;
-
-	ft::tree<int>::node* n = tree.find(18);
-	n->leftRotate();
-	tree.print();
-
-	std::cout << std::endl << std::endl;
-
-	n = tree.find(3);
-	n->rightRotate();
-	tree.print();
-
 
 	return 0;
 }
